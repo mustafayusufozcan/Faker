c228749ff9ecfdcea10dc75420c32387d53a7217
Mustafa Yusuf Özcan <mustafayusufozcan@gmail.com>
2025-05-17 01:28:04 +0300

    Fix negative tenth digit issue

--- src/Provider/tr_TR/Person.php
+++ src/Provider/tr_TR/Person.php
@@ -139,7 +139,7 @@ class Person extends \Faker\Provider\Person
             }
         }
 
-        $tenthDigit = (7 * $evenSum - $oddSum) % 10;
+        $tenthDigit = ((7 * $evenSum - $oddSum) % 10 + 10) % 10;
         $eleventhDigit = ($evenSum + $oddSum + $tenthDigit) % 10;
 
         return $tenthDigit . $eleventhDigit;
